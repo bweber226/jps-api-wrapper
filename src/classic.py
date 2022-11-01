e9fe24380d73e6025955d12a0674ac7065e5792c
bweber26 <bweber26@cvtc.edu>
2022-11-01 15:29:44 -0500

    Version 0.7.0, /advancedmobiledevicesearches endpoints added

--- src/classic.py
+++ src/classic.py
@@ -304,6 +304,101 @@ class Classic(RequestBuilder):
     /advancedmobiledevicesearches
     """
 
+    def get_advanced_mobile_device_searches(
+        self, data_type: str = "json"
+    ) -> Union[dict, str]:
+        """
+        Returns all advanced mobile device searches in either JSON or XML.
+
+        :param data_type: JSON or XML
+        """
+        endpoint = "/JSSResource/advancedmobiledevicesearches"
+
+        return self._get(endpoint, data_type)
+
+    def get_advanced_mobile_device_search(
+        self, data_type: str = "json", id: Union[int, str] = None, name: str = None
+    ) -> Union[dict, str]:
+        """
+        Returns data on a specific advanced mobile device search by either ID or
+        name.
+
+        :param data_type: JSON or XML
+        :param id: Advanced mobile device search ID
+        :param name: Advanced mobile device search name
+        """
+        identification_options = {
+            "id": id,
+            "name": name,
+        }
+        identification = identification_type(identification_options)
+        endpoint = (
+            f"/JSSResource/advancedmobiledevicesearches/{identification}/"
+            f"{identification_options[identification]}"
+        )
+
+        return self._get(endpoint, data_type)
+
+    def create_advanced_mobile_device_search(
+        self, data: str, id: Union[int, str] = 0
+    ) -> str:
+        """
+        Creates an advanced mobile device search with the given XML data. Use ID 0
+        to use the next available ID.
+
+        :param data: XML data to create the advanced mobile device search with
+        :param id:
+            ID of the new advanced mobile device search, use 0 for next available ID
+        """
+        endpoint = f"/JSSResource/advancedmobiledevicesearches/id/{id}"
+
+        return self._post(endpoint, data, data_type="xml")
+
+    def update_advanced_mobile_device_search(
+        self, data: str, id: Union[int, str] = None, name: str = None
+    ) -> str:
+        """
+        Updates and advanced mobile device search with the given XML data. Need to
+        supply at least one identifier.
+
+        :param data: XML data to update the advanced mobile device search with
+        :param id: Advanced mobile device search ID
+        :param name: Advanced mobile device search name
+        """
+        identification_options = {
+            "id": id,
+            "name": name,
+        }
+        identification = identification_type(identification_options)
+        endpoint = (
+            f"/JSSResource/advancedmobiledevicesearches/{identification}/"
+            f"{identification_options[identification]}"
+        )
+
+        return self._put(endpoint, data, data_type="xml")
+
+    def delete_advanced_mobile_device_search(
+        self, id: Union[int, str] = None, name: str = None
+    ) -> Union[dict, str]:
+        """
+        Deletes an advanced mobile device search by either ID or name. Need to supply
+        at least one identifier.
+
+        :param id: Advanced mobile device search ID
+        :param name: Advanced mobile device search name
+        """
+        identification_options = {
+            "id": id,
+            "name": name,
+        }
+        identification = identification_type(identification_options)
+        endpoint = (
+            f"/JSSResource/advancedmobiledevicesearches/{identification}/"
+            f"{identification_options[identification]}"
+        )
+
+        return self._delete(endpoint, data_type="xml")
+
     """
     /advancedusersearches
     """
