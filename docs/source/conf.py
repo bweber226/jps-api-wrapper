e312709ddf3acfdc6d9e7564183173c01c97f78f
Bryan Weber <bweber26@cvtc.edu>
2023-01-27 01:22:39 +0000

    Revert "v1.2.3, updated gitlab CI config"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2023, Bryan Weber"
 author = "Bryan Weber"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.3"
+release = "1.2.2"
 
 
 # -- General configuration ---------------------------------------------------
